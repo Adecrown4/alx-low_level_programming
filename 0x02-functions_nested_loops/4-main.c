673a2806850bfe6766c32672603824c7a1edafe9
Adecrown4 <adecrown2030@gmail.com>
2022-09-15 01:08:34 -0700

    Task 4

--- 0x02-functions_nested_loops/4-main.c
+++ 0x02-functions_nested_loops/4-main.c
@@ -12,11 +12,15 @@ int main(void)
 	r = _isalpha('H');
 	_putchar(r + '0');
 	r = _isalpha('o');
-	_putchar(r + '0');
+	r = _isalpha('c');
 	r = _isalpha(108);
-	_putchar(r + '0');
+	r = _isalpha('i');
 	r = _isalpha(';');
-	_putchar(r + '0');
+	r = _isalpha('s');
+	r = _isalpha('F');
+	r = _isalpha('9');
+	r = _isalpha('M');
+	r = _isalpha('&');
 	_putchar('\n');
 	return (0);
 }
