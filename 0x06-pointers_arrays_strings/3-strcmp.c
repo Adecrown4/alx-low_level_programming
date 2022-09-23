1413b5fd35c121766c2d1c51979fcf18a030457b
Adecrown4 <adecrown2030@gmail.com>
2022-09-23 06:00:35 -0700

    Task 4

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+int _strcmp(char *s2, char *s2)
+{
+	int i = 0, diff = 0;
+
+	while (1)
+	{
+		if (s2[i] == '\0' && s2[i] == '\0')
+			break;
+		else if (s2[i] == '\0')
+		{
+			diff = s2[i];
+			break;
+		}
+		else if (s2[i] == '\0')
+		{
+			diff = s2[i];
+			break;
+		}
+		else if (s2[i] != s2[i])
+		{
+			diff = s2[i] - s2[i];
+			break;
+		}
+		else
+			i++;
+	}
+	return (diff);
+}
